372eabadf00b6c5110fd82ca627aa5fc483cec20
Dominik Schilling <dominikschilling+git@gmail.com>
2013-05-04 00:54:41 +0200

    Adjust test client/server to latest changes

--- SerienTracker/src/minirestwebservice/SeriesService.java
+++ SerienTracker/src/minirestwebservice/SeriesService.java
@@ -5,19 +5,20 @@ import java.io.File;
 import javax.ws.rs.*;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
+import jaxb.ObjectFactory;
+import jaxb.Serie;
+import jaxb.Series;
 
-import jaxb.series.*;
 
-@Path( "/series/{id}" )
+@Path( "/series" )
 public class SeriesService {
 
 	private Unmarshaller unMarshaller;
 
 	@GET @Produces( "application/xml" )
-	public Series getAll(@PathParam("id") String id) throws JAXBException {
+	public Series getAll() throws JAXBException {
 		ObjectFactory of = new ObjectFactory();
 
 		Series series = of.createSeries();
@@ -25,9 +26,24 @@ public class SeriesService {
 		JAXBContext jaxbContext = JAXBContext.newInstance( Series.class );
 
 		this.unMarshaller = jaxbContext.createUnmarshaller(); // Reading
-
-		series = (Series) unMarshaller.unmarshal( new File( "XML Examples/Serie" + id + ".xml" ) );
+		series = (Series) unMarshaller.unmarshal( new File( "XML Examples/Series.xml" ) );
 
 		return series;
 	}
+
+	@Path( "/{id}" )
+	@GET @Produces( "application/xml" )
+	public Serie getSingle(@PathParam("id") int id) throws JAXBException {
+		ObjectFactory of = new ObjectFactory();
+
+		Series series = of.createSeries();
+
+		JAXBContext jaxbContext = JAXBContext.newInstance( Series.class );
+
+		this.unMarshaller = jaxbContext.createUnmarshaller(); // Reading
+		series = (Series) unMarshaller.unmarshal( new File( "XML Examples/Series.xml" ) );
+
+		return series.getSerie().get(id);
+	}
+
 }
--- SerienTracker/src/minirestwebservice/TestClient.java
+++ SerienTracker/src/minirestwebservice/TestClient.java
@@ -5,11 +5,18 @@ import com.sun.jersey.api.client.*;
 public class TestClient {
 	public static void main( String[] args ) {
 
-		String url = "http://localhost:4434/series/1";
+		String url = "http://localhost:4434/series/";
 		System.out.println( "URL: " + url );
 
 		WebResource wrs = Client.create().resource( url );
 
 		System.out.println( wrs.accept( "application/xml" ).get( String.class ) );
+
+		String url2 = "http://localhost:4434/series/1";
+		System.out.println( "URL: " + url2 );
+
+		WebResource wrs2 = Client.create().resource( url2 );
+
+		System.out.println( wrs2.accept( "application/xml" ).get( String.class ) );
 	}
 }
