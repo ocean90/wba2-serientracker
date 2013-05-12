3ff1a4b0e367db7728d08214ff2535f39f6a7fb7
Dominik Schilling <dominikschilling+git@gmail.com>
2013-05-12 13:04:37 +0200

    * Use a foreach loop to get the requested series id object. * Implement a DELETE endpoint to delete a series.

--- SerienTracker/src/minirestwebservice/SeriesService.java
+++ SerienTracker/src/minirestwebservice/SeriesService.java
@@ -1,15 +1,14 @@
 package minirestwebservice;
 
 import java.io.File;
+import java.util.List;
 
 import javax.ws.rs.*;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
-import org.apache.commons.jxpath.JXPathContext;
-
-import jaxb.ObjectFactory;
 import jaxb.Serie;
 import jaxb.Series;
 
@@ -18,17 +17,15 @@ import jaxb.Series;
 public class SeriesService {
 
 	private Unmarshaller unMarshaller;
+	private Marshaller marshaller;
+	private final File file = new File( "XML Examples/Series.xml" );
 
 	@GET @Produces( "application/xml" )
 	public Series getAll() throws JAXBException {
-		ObjectFactory of = new ObjectFactory();
-
-		Series series = of.createSeries();
-
 		JAXBContext jaxbContext = JAXBContext.newInstance( Series.class );
 
 		this.unMarshaller = jaxbContext.createUnmarshaller(); // Reading
-		series = (Series) unMarshaller.unmarshal( new File( "XML Examples/Series.xml" ) );
+		Series series = (Series) unMarshaller.unmarshal( this.file );
 
 		return series;
 	}
@@ -36,22 +33,46 @@ public class SeriesService {
 	@Path( "/{id}" )
 	@GET @Produces( "application/xml" )
 	public Serie getSingle(@PathParam("id") int id) throws JAXBException {
-		ObjectFactory of = new ObjectFactory();
-
-		Series series = of.createSeries();
 
 		JAXBContext jaxbContext = JAXBContext.newInstance( Series.class );
 
 		this.unMarshaller = jaxbContext.createUnmarshaller(); // Reading
-		series = (Series) unMarshaller.unmarshal( new File( "XML Examples/Series.xml" ) );
+		Series rawSeries = (Series) unMarshaller.unmarshal( this.file );
 
-		JXPathContext context = JXPathContext.newContext(series);
-		
-		Serie serie = (Serie) context.getValue( "serie/[1]");
-		return serie;
-		//Serie serie = jaxbContext.newContext() //.getValueByXPath(series, "@id", null, Serie.class);
-		
-		//return series.getSerie().get(id);
+		List<Serie> series = rawSeries.getSerie();
+
+		for ( Serie serie : series ) {
+			if ( serie.getSerieID().intValue() == id ) {
+				return serie;
+			}
+		}
+
+		return null;
+	}
+
+	@Path( "/{id}" )
+	@DELETE @Produces( "application/xml" )
+	public String deleteSingle(@PathParam("id") int id) throws JAXBException {
+
+		JAXBContext jaxbContext = JAXBContext.newInstance( Series.class );
+
+		this.unMarshaller = jaxbContext.createUnmarshaller(); // Reading
+		this.marshaller   = jaxbContext.createMarshaller(); // Writing
+		this.marshaller.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, true );
+		Series rawSeries = (Series) unMarshaller.unmarshal( this.file );
+
+		List<Serie> series = rawSeries.getSerie();
+
+		for ( Serie serie : series ) {
+			if ( serie.getSerieID().intValue() == id ) {
+				series.remove( serie );
+				this.marshaller.marshal( rawSeries, this.file );
+				return "<success>1</success";
+			}
+
+		}
+
+		return "<success>0</success";
 	}
 
 }
--- SerienTracker/src/minirestwebservice/TestClient.java
+++ SerienTracker/src/minirestwebservice/TestClient.java
@@ -8,15 +8,22 @@ public class TestClient {
 		String url = "http://localhost:4434/series/";
 		System.out.println( "URL: " + url );
 
-		/*WebResource wrs = Client.create().resource( url );
+		WebResource wrs = Client.create().resource( url );
 
 		System.out.println( wrs.accept( "application/xml" ).get( String.class ) );
-		*/
+
 		String url2 = "http://localhost:4434/series/1";
 		System.out.println( "URL: " + url2 );
 
 		WebResource wrs2 = Client.create().resource( url2 );
 
 		System.out.println( wrs2.accept( "application/xml" ).get( String.class ) );
+
+		String url3 = "http://localhost:4434/series/2";
+		System.out.println( "URL: " + url3 );
+
+		WebResource wrs3 = Client.create().resource( url3 );
+
+		System.out.println( wrs3.accept( "application/xml" ).delete( String.class ) );
 	}
 }
