1aec5429e0ee4b03ef6f0f92777c9389465ae1fb
Dominik Schilling <dominikschilling+git@gmail.com>
2013-06-13 13:34:35 +0200

    Logger: Codestyling

--- SerienTracker/src/de/fhkoeln/gm/serientracker/utils/Logger.java
+++ SerienTracker/src/de/fhkoeln/gm/serientracker/utils/Logger.java
@@ -7,47 +7,47 @@ import java.util.Calendar;
 public class Logger {
 
 	public enum ErrorType {
-		 E_NONE( 0 ), E_ERROR( 1 ), E_ALL( 2 );
+		E_NONE( 0 ), E_ERROR( 1 ), E_ALL( 2 );
 
-		 private int code;
+		private int code;
 
-		 private ErrorType( int c ) {
-			 code = c;
-		 }
+		private ErrorType( int c ) {
+			code = c;
+		}
 
-		 public int getCode() {
-			 return code;
-		 }
+		public int getCode() {
+			return code;
+		}
 	}
 
-	public static ErrorType enabled = ErrorType.E_ALL;
+	public static ErrorType errortype = ErrorType.E_ALL;
 
 	private Logger() {}
 
 	public static void log( String message ) {
-		if ( enabled.getCode() == 2 )
+		if ( errortype.getCode() == 2 )
 			System.out.printf(
-					"%s: %s::%s() - %s\n",
-					getDateTime(),
-					getClassName(),
-					getMethodName(),
-					message
+				"%s: %s::%s() - %s\n",
+				getDateTime(),
+				getClassName(),
+				getMethodName(),
+				message
 			);
 	}
 
 	public static void err( String message ) {
-		if ( enabled.getCode() == 1 || enabled.getCode() == 2 )
+		if ( errortype.getCode() == 1 || errortype.getCode() == 2 )
 			System.err.printf(
-					"%s: %s::%s() - %s\n",
-					getDateTime(),
-					getClassName(),
-					getMethodName(),
-					message
+				"%s: %s::%s() - %s\n",
+				getDateTime(),
+				getClassName(),
+				getMethodName(),
+				message
 			);
 	}
 
 	private static String getDateTime() {
-		return new SimpleDateFormat( "yyyy.MM.dd HH:mm:ss.SSS").format( Calendar.getInstance().getTime() );
+		return new SimpleDateFormat( "yyyy.MM.dd HH:mm:ss.SSS" ).format( Calendar.getInstance().getTime() );
 	}
 
 	private static String getMethodName() {
